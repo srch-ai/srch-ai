bdb50d8e4591e4dffe124f82bbe6563824336c3f
Scot Clausing <tsclausing@gmail.com>
2023-07-23 14:22:07 -0500

    Upgrade Svelte/kit and add Flowbite

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,23 @@
-<h1>Welcome to Srch AI</h1>
-<p>Hosted on Vercel</p>
+<script>
+	import { Navbar, NavBrand, NavLi, NavUl, NavHamburger, Button } from 'flowbite-svelte';
+</script>
+
+<Navbar let:hidden let:toggle>
+	<NavBrand href="/">
+		<img src="/srch-ai.png" class="mr-3 h-6 sm:h-9" alt="Flowbite Logo" />
+		<span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
+			Srch AI
+		</span>
+	</NavBrand>
+	<div class="flex md:order-2">
+		<Button size="sm">Get at it</Button>
+		<NavHamburger on:click={toggle} />
+	</div>
+	<NavUl {hidden} class="order-1">
+		<NavLi href="/" active={true}>Base</NavLi>
+		<NavLi href="/vibe">Vibe</NavLi>
+		<NavLi href="/facts">Facts</NavLi>
+		<NavLi href="/pricing">Pricing</NavLi>
+		<NavLi href="/contact">Contact</NavLi>
+	</NavUl>
+</Navbar>
